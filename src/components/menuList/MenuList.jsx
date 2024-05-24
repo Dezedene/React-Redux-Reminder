024b1c9c956b05fc35a37470c2b95d0515ad13d0
Dezedene <gregoire.suray@gmail.com>
2024-05-24 15:26:48 -0400

    improve code

--- src/components/menuList/MenuList.jsx
+++ src/components/menuList/MenuList.jsx
@@ -1,29 +1,18 @@
 import MenuListItems from "../menuListItem/MenuListItem";
+import { DIFFICULTIES } from "./constant";
 import s from "./style.module.css";
 
 const MenuList = ({ difficulty, onItemClick }) => {
   return (
     <div className={s.container}>
-      <MenuListItems
-        onClick={onItemClick}
-        difficulty="Low"
-        isSelected={difficulty === "Low"}
-      />
-      <MenuListItems
-        onClick={onItemClick}
-        difficulty="Medium"
-        isSelected={difficulty === "Medium"}
-      />
-      <MenuListItems
-        onClick={onItemClick}
-        difficulty="High"
-        isSelected={difficulty === "High"}
-      />
-      <MenuListItems
-        onClick={onItemClick}
-        difficulty="Insane"
-        isSelected={difficulty === "Insane"}
-      />
+      {DIFFICULTIES.map((diff, i) => (
+        <MenuListItems
+          key={i}
+          onClick={onItemClick}
+          difficulty={diff}
+          isSelected={difficulty === diff}
+        />
+      ))}
     </div>
   );
 };
